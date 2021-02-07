2cf1acabd262d1db59fd8495533e670418b24fcb
oilbean <1057260346@qq.com>
2021-02-07 18:37:23 +0800

    Update test.py

--- test.py
+++ test.py
@@ -22,8 +22,8 @@ import logging.config
 # with open(os.getcwd() + '/Template/' + 'recevers.txt', 'r') as f:
 #     recevers = f.read().splitlines()
 
-jira = JIRA(basic_auth=('xiaomenghong@creditease.cn', 'meng12..'),
-                  options={'server': 'http://jira.creditease.corp/'})
+jira = JIRA(basic_auth=('', ''),
+                  options={'server': '/'})
 print(jira)
 
 # jira_caifu = JIRA("http://10.143.143.167:18085/", basic_auth=('admin', 'fso2016'))
